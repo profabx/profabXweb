ef95f8d32ad4ee7d1d52e8916cd31c661bd62634
Stan <451388360@qq.com>
2023-12-10 17:44:27 +0800

    ✨ feat: 增加侧边栏数据

--- src/constants.ts
+++ src/constants.ts
@@ -484,4 +484,28 @@ const MenuData = [
   },
 ];
 
-export { MetallicMaterialsData, NonMetallicMaterialsData, MenuData };
+// 侧边栏侧栏
+const SideBarData = [
+  // A single link item labelled “Home”.
+  { label: "Home", link: "/" },
+  // A group labelled “Start Here” containing two links.
+  {
+    label: "Start Here",
+    items: [
+      { label: "Introduction", link: "/intro" },
+      { label: "Next Steps", link: "/next-steps" },
+    ],
+  },
+  // A group linking to all pages in the reference directory.
+  {
+    label: "Reference",
+    autogenerate: { directory: "reference" },
+  },
+];
+
+export {
+  MetallicMaterialsData,
+  NonMetallicMaterialsData,
+  MenuData,
+  SideBarData,
+};
