9541ee044956edea1bcf07a2dbb67a5bc0d4a2d0
Xu sun <xu.sun@nottingham.edu.cn>
2025-02-15 02:59:57 +0800

    test color

--- src/datas/TableData.ts
+++ src/datas/TableData.ts
@@ -44,7 +44,7 @@ const TableData = [
       polymersData["ABS"],polymersData["PP"],polymersData["尼龙"], polymersData["PEEK"],polymersData["TPU"],polymersData["PLA"],polymersData["ASA"],componsitesData["PACF"],componsitesData["PAGF"],componsitesData["ABSCF"],componsitesData["PLA+wood"],
      
     ],
-    right: [metalData["铁/钢"], componsitesData["陶瓷"],],
+    right: [metalData["铁/钢"], ceramicsData["陶瓷"],],
   },
   //lcd/dlp
   {
